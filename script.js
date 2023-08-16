6e6d116a3ff20ce9f2a8d306d0464d899fed64e7
Dylan-Gresham <dgresh724@gmail.com>
2023-08-15 21:02:09 -0600

    Frame the page
    Created a "header" which has the Player One and Player Two names. Player
    One will always have tile 'O' and Player Two will always have tile 'X'.
    
    Left a space in the middle for the actual game board. I think I'll make
    that div be a grid and have the first row be 1fr and the second be 3fr.
    The first row will be a status bar showing W/L/T for each player and the
    current round that the game is on.
    
    There's also a footer on the bottom that links back to the GitHub repo
    for this project.

---
+++ script.js
@@ -0,0 +1,61 @@
+const gbRowOne = document.getElementById('boardRowOne');
+const gbRowTwo = document.getElementById('boardRowTwo');
+const gbRowThree = document.getElementById('boardRowThree');
+const gbRows = [gbRowOne, gbRowTwo, gbRowThree];
+
+const Game = function(PlayerOne, PlayerTwo) {
+    this.playerOne = PlayerOne;
+    this.playerTwo = PlayerTwo;
+    this.rounds = 0;
+    this.gameBoard = Gameboard();
+
+    this.showGame = () => {
+        for(let i = 0; i < 3; i++) {
+            for(let j = 0; j < 3; j++) {
+                gbRows[i].appendChild(this.gameBoard.board[i, j]);
+            }
+        }
+    };
+
+    this.endGame = () => {
+        if(this.playerOne.wins > this.playerTwo.wins) {
+            // Do something for when Player One wins
+        } else if(this.playerOne.wins < this.playerTwo.wins) {
+            // Do something for when Player Two wins
+        } else {
+            // Do something for when it's a tie
+        }
+    }
+
+    this.incrementRounds = () => this.rounds++;
+}
+
+const Player = function(xOrO, name) {
+    this.tile = xOrO;
+    this.wins = 0;
+    this.losses = 0;
+    this.ties = 0;
+    this.name = name;
+    
+    this.incrementWins = () => this.wins++;
+    this.incrementLosses = () => this.losses++;
+    this.incrementTies = () => this.ties++;
+}
+
+const Gameboard = function() {
+    this.board = [
+        [/* Three buttons as tiles */],
+        [/* Three buttons as tiles */],
+        [/* Three buttons as tiles */]
+    ];
+    this.winner = NaN; // 0 if O wins, 1 if X wins, 2 if tie
+    this.gameState = "start";
+
+    this.changeState = (state) => this.gameState = state;
+    this.resetBoard = () => {
+        // Reset this.board here
+
+        this.winner = NaN;
+        this.gameState = "start";
+    };
+}
