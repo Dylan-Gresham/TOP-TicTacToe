b798cbdd5939637116a99d0132b79fd92f20c0b0
Dylan-Gresham <dgresh724@gmail.com>
2023-08-18 13:39:34 -0600

    See description of this commit
    I got the visuals of the page improved a little more and added the
    boxes on the left and right that'll eventually denote who's turn it is
    but as of right now they're statically colored red.
    
    I also implemented the buttons for the most part. The turns switch
    properly but I still need to figure out how I want to get the colors to
    change based on who's turn it is.
    
    The big issue right now is the `detectWin()` function. As of right now
    it detects horizontal wins, doesn't detect vertical, and diagonal is
    untested. I'm not sure why it's not detecting vertical wins though
    which is the biggest issue.
    
    Will look more in depth at the `detectWin()` function later.

--- script.js
+++ script.js
@@ -12,16 +12,19 @@ const playerXWins = document.getElementById('playerXWins');
 const playerXLosses = document.getElementById('playerXLosses');
 const playerXTies = document.getElementById('playerXTies');
 
+const gbContainer = document.getElementById('boardContainer');
 const gbRowOne = document.getElementById('boardRowOne');
 const gbRowTwo = document.getElementById('boardRowTwo');
 const gbRowThree = document.getElementById('boardRowThree');
 const gbRows = [gbRowOne, gbRowTwo, gbRowThree];
 
+currentTurn = 'o';
+
 const Game = function(PlayerOne, PlayerTwo) {
     this.playerOne = PlayerOne;
     this.playerTwo = PlayerTwo;
     this.rounds = 0;
-    this.gameBoard = Gameboard();
+    this.gameBoard = Gameboard(this);
 
     this.showGame = () => {
         playerO.textContent = this.playerOne.name;
@@ -43,14 +46,8 @@ const Game = function(PlayerOne, PlayerTwo) {
         }
     };
 
-    this.endGame = () => {
-        if(this.playerOne.wins > this.playerTwo.wins) {
-            // Do something for when Player One wins
-        } else if(this.playerOne.wins < this.playerTwo.wins) {
-            // Do something for when Player Two wins
-        } else {
-            // Do something for when it's a tie
-        }
+    this.endGame = (winnerStr) => {
+        console.log('win! ' + winnerStr);
     }
 
     this.incrementRounds = () => {
@@ -75,7 +72,7 @@ const Player = function(xOrO, name) {
     return {wins, losses, ties, name, incrementWins, incrementLosses, incrementTies};
 }
 
-const Gameboard = function() {
+const Gameboard = function(game) {
     this.board = [
         [],
         [],
@@ -86,6 +83,21 @@ const Gameboard = function() {
         for(let j = 0; j < 3; j++) {
             const button = document.createElement('button');
             button.classList.add('tile');
+            button.addEventListener('click', event => {
+                if(button.textContent !== '') {
+                    event.stopPropagation();
+                    
+                    return;
+                } else if(currentTurn === 'o') {
+                    button.textContent = 'O';
+                } else {
+                    button.textContent = 'X';
+                }
+
+                detectWin();
+                switchTurns();
+            });
+
             this.board[i].push(button);
         }
     }
@@ -104,6 +116,16 @@ const Gameboard = function() {
     return {board, changeState, resetBoard};
 }
 
+function switchTurns() {
+    if(currentTurn === 'o') {
+        currentTurn = 'x';
+    } else {
+        currentTurn = 'o';
+    }
+
+    return;
+}
+
 playButton.addEventListener('click', (event) => {
     let oName = `${playerOName.value}:`;
     playerOName.disabled = true;
@@ -112,7 +134,57 @@ playButton.addEventListener('click', (event) => {
     let playerOTemp = Player('o', oName);
     let playerXTemp = Player('x', xName);
     let game = Game(playerOTemp, playerXTemp);
+    gbRowTwo.classList.toggle('addTiles');
     game.showGame();
 
     event.stopPropagation();
 });
+
+function detectWin() {
+    let rowOne = [];
+    let rowTwo = [];
+    let rowThree = [];
+    let rows = [rowOne, rowTwo, rowThree];
+
+    // Get all the rows 
+    for(let i = 0; i < 3; i++) {
+        for(let j = 0; j < 3; j++) {
+            rows[i][j] = gbRows[i].childNodes[j + 1].textContent;
+        }
+    }
+
+    // Check for horizontal wins
+    for(let i = 0; i < 3; i++) {
+        if(rows[i][0] !== '' && (rows[i][0] === rows[i][1] && rows[i][1] === rows[i][2])) {
+            endGame(rows[i][0]);
+
+            return;
+        }
+    }
+
+    // Check for vertical wins
+    if(rowOne[0] !== '' && (rowOne[0] === rowTwo[0] === rowThree[0])) {
+        endGame(rowOne[0]);
+
+        return;
+    } else if(rowOne[1] !== '' && (rowOne[1] === rowTwo[1] === rowThree[1])) {
+        endGame(rowOne[1]);
+
+        return;
+    } else if(rowOne[2] !== '' && (rowOne[2] === rowTwo[2] === rowThree[2])) {
+        endGame(rowOne[2]);
+
+        return;
+    }
+
+    // Check for diagonal wins
+    if(rowOne[0] !== '' && (rowOne[0] === rowTwo[1] === rowThree[2])) {
+        endGame(rowOne[0]);
+
+        return;
+    } else if(rowOne[2] !== '' && (rowOne[2] === rowTwo[1] === rowThree[0])) {
+        endGame(rowOne[2]);
+
+        return;
+    }
+};
